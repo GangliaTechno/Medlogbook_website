48e53f8026285e2c4a96ca348d54095d6b631d77
nithin04444 <155780776+nithin04444@users.noreply.github.com>
2025-07-25 15:05:57 +0530

    some think

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://medlogbook.ganglia.in', 'https://medlogbook-website-frontend.onrender.com'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://medlogbook.ganglia.in', 'https://medlogbook-website-frontend.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
